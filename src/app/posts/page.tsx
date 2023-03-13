d689e7eec9b36b458d907c8d4c1a8ce94cad2979
Nicolas Toffolo <nicolas.toffolo@mirahi.io>
2023-03-13 09:42:50 +0100

    Added SSG

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -11,3 +11,15 @@ export default async function PostPage() {
         </main>
     );
 }
+
+/* 
+ This function allows us to generate static pages (Static Site Generation)
+ See: https://youtu.be/E1HzFvXgrCs?t=525
+ */
+export async function generateStaticParams() {
+    const posts = await fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(res => res.json() as Promise<PostType[]>)
+        .catch(err => console.error(err));
+
+    return posts ? posts.map(post => ({ params: { id: post.id.toString() } })) : [];
+}
